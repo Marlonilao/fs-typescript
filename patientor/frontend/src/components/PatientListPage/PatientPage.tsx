5e1011aa174c016d48984bb5f9e091903f15caf6
Marlonilao <176732593+Marlonilao@users.noreply.github.com>
2026-06-06 13:21:25 +0800

    add exercise 24

---
+++ patientor/frontend/src/components/PatientListPage/PatientPage.tsx
@@ -0,0 +1,39 @@
+import { useEffect, useState } from 'react';
+import { useParams } from 'react-router-dom';
+import patientService from '../../services/patients';
+import type { Patient } from '../../types';
+
+const PatientPage = () => {
+  const { id } = useParams<{ id: string }>();
+  const [patient, setPatient] = useState<Patient | null>(null);
+
+  useEffect(() => {
+    const fetchPatient = async () => {
+      const patient = await patientService.getPatientInfo(id!);
+      setPatient(patient);
+      if (patient) {
+        console.log('Success fetching patient info');
+      }
+    };
+
+    fetchPatient();
+    return () => {
+      setPatient(null);
+      console.log('set patient to null');
+    };
+  }, [id]);
+
+  if (patient) {
+    return (
+      <div>
+        <h1>{patient.name}</h1>
+        <p>Gender: {patient.gender}</p>
+        <p>SSN: {patient.ssn}</p>
+        <p>Occupation: {patient.occupation}</p>
+        <p>Date of Birth: {patient.dateOfBirth}</p>
+      </div>
+    );
+  }
+};
+
+export default PatientPage;
